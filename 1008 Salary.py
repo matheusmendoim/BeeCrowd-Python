ff90b789afbdd0e3ec9ab50afa7dc947f5f54307
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-01-11 10:57:58 -0300

    Solução 1008

---
+++ 1008 Salary.py
@@ -0,0 +1,8 @@
+numero_empregado = int(input())
+horas_trabalhadas = int(input())
+preco_hora = float(input())
+
+salario = horas_trabalhadas * preco_hora
+
+print(f"NUMBER = {numero_empregado}")
+print(f"SALARY = U$ {salario:.2f}")
