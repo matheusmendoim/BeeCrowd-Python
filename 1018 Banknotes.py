280db554e2ebfe36aa358390e0aa57b21a1677d7
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-01-11 15:30:47 -0300

    Solução 1018

--- 1018 Banknotes.py
+++ 1018 Banknotes.py
@@ -1,17 +1,31 @@
-def troco(valor, notas_em_x):
-    notas = valor//notas_em_x
-    valor %= notas_em_x
-    return notas
-
-
 valor = int(input())
 
 print(valor)
 
-print(f"""{troco(valor, 100)} nota(s) de R$ 100,00
-{troco(valor, 50)} nota(s) de R$ 50,00
-{troco(valor, 25)} nota(s) de R$ 25,00
-{troco(valor, 10)} nota(s) R$ 10,00
-{troco(valor, 5)} nota(s) de R$ 5,00
-{troco(valor, 2)} nota(s) de R$ 2,00
-{troco(valor, 1)} nota(s) de R$ 1,00""")
+notas_100 = valor//100
+valor %= 100
+
+notas_50 = valor//50
+valor %= 50
+
+notas_25 = valor//20
+valor %= 20
+
+notas_10 = valor//10
+valor %= 10
+
+notas_5 = valor//5
+valor %= 5
+
+notas_2 = valor//2
+valor %= 2
+
+notas_1 = valor//1
+
+print(f"""{notas_100} nota(s) de R$ 100,00
+{notas_50} nota(s) de R$ 50,00
+{notas_25} nota(s) de R$ 20,00
+{notas_10} nota(s) de R$ 10,00
+{notas_5} nota(s) de R$ 5,00
+{notas_2} nota(s) de R$ 2,00
+{notas_1} nota(s) de R$ 1,00""")
