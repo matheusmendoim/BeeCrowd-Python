b5dd98a4eaf3f7c47d6cffef07e8c6cab1781ddf
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-01-13 20:09:36 -0300

    Solução 1035

--- 1035 Selection Test 1.py
+++ 1035 Selection Test 1.py
@@ -1,14 +1,11 @@
 a, b, c, d = map(int, input().split())
 confirmador = False
 
-if b > c and d > a:
-    confirmador == True
-if (c+d)>(a+c):
-    confirmador == True
-if c > 0 and d > 0:
-    confirmador == True
-if a % 2 == 0:
-    confirmador == True
+if b>c and d>a:
+    if (c+d)>(a+b):
+        if c>0 and d>0:
+            if a%2==0:
+                confirmador = True
 
 if confirmador == True:
     print("Valores aceitos")
