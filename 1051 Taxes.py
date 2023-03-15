21646674c3962428e59d01de4a33a6b0eebcff44
matheusmendoim <matheusinho.mfsm@gmail.com>
2023-03-15 19:18:44 -0300

    Solução 1051

--- 1051 Taxes.py
+++ 1051 Taxes.py
@@ -12,15 +12,16 @@ else:
 
     elif salary > 3000 and salary <= 4500:
         salary -= 2000
-        resto = salary-1000
-        taxe += (resto*(18/100))
-        taxe += (salary-resto)*(8/100)
+        rest = salary-1000
+        taxe += (rest*(18/100))
+        taxe += ((salary-rest)*(8/100))
 
     else:
         salary -= 2000
-        resto = salary-1000
-        taxe += (resto*(18/100))
-        taxe += (salary-res
-        to)*(8/100)
-
+        rest = salary-1000
+        rest2 = salary-2500
+        taxe += rest2*(28/100)
+        taxe += abs(rest-rest2)*(18/100)
+        taxe += abs(salary-rest)*(8/100)
+        
     print(f"R$ {taxe:.2f}")
